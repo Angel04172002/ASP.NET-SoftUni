da71e07d3d76dcbe36dd8eaea495b2b091424e4f
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-02 23:14:26 +0200

    Add files via upload

---
+++ ASP.NET Fundamentals/1. ASP.NET Core Introduction/Chat App/Controllers/ChatController.cs
@@ -0,0 +1,54 @@
+﻿using ChatApp.Models.Chat;
+using ChatApp.Models.Message;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> messages
+            = new List<KeyValuePair<string, string>>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Show()
+        {
+            if(messages.Count <= 0)
+            {
+                return View(new ChatViewModel());
+            }
+
+
+            var chatModel = new ChatViewModel()
+            {
+                Messages = messages
+                    .Select(m => new MessageViewModel()
+                    {
+                        Sender = m.Key,
+                        MessageText = m.Value
+                    })
+                    .ToList()
+            };
+
+            return View(chatModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat) 
+        {
+            var newMessage = chat.CurrentMessage;
+
+            messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.MessageText));
+
+            return RedirectToAction(nameof(Show));
+        }
+       
+    }
+}
---
+++ ASP.NET Fundamentals/1. ASP.NET Core Introduction/Chat App/Models/Chat/ChatViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ChatApp.Models.Message;
+
+namespace ChatApp.Models.Chat
+{
+    public class ChatViewModel
+    {
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+
+        public List<MessageViewModel> Messages { get; set; } = null!;
+    }
+}
