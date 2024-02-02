1ffeded5436bdf4ab1c77ed7abdaf9061d0c9829
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-02 21:46:28 +0200

    Delete ASP.NET Fundamentals/Asp.Net Core MVC Intro directory

--- ASP.NET Fundamentals/Asp.Net Core MVC Intro/wwwroot/js/numbers.js
+++
@@ -1,5 +0,0 @@
-function getLimitedNumbers() {
-
-    let inputValue = document.getElementById("numberInput").value;
-    window.location = "https://localhost:7127/Numbers/Limit?num=" + inputValue;
-}
