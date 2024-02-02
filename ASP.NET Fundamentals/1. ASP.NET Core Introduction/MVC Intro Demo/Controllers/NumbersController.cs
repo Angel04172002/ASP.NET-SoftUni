c86d6fdaa31588653adc6df28ceeeeef68de98a2
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-02 21:54:23 +0200

    Add files via upload

---
+++ ASP.NET Fundamentals/1. ASP.NET Core Introduction/MVC Intro Demo/Controllers/NumbersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcIntroDemo.Controllers
+{
+    public class NumbersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Limit(int num)
+        {
+            return View(num);
+        }
+    }
+}
