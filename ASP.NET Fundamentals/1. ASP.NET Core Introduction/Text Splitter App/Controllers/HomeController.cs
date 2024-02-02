1dd424ad4aca1727468f3d38d6c67fcc02af8a43
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-02 23:44:03 +0200

    Add files via upload

---
+++ ASP.NET Fundamentals/1. ASP.NET Core Introduction/Text Splitter App/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Text_Splitter.Models;
+
+namespace Text_Splitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextViewModel model)
+        {
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            var splitTextArr = model.Text
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplitText = string.Join(Environment.NewLine, splitTextArr);
+
+            return RedirectToAction(nameof(Index), model);
+        }
+    }
+}
---
+++ ASP.NET Fundamentals/1. ASP.NET Core Introduction/Text Splitter App/Models/TextViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Text_Splitter.Models
+{
+    public class TextViewModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        public string Text { get; set; } = string.Empty;
+
+        public string SplitText { get; set; } = string.Empty;
+
+
+    }
+}
