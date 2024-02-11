a4bf843e332a7c95747baf4d9b266ce0ee2d4594
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-11 14:13:54 +0200

    Add files via upload

---
+++ ASP.NET Fundamentals/3. State Management and Asynchronous Processing/State Management Demo/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StateManagement.Models;
+using System.Diagnostics;
+using System.Text;
+
+namespace StateManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+      
+
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            this.HttpContext.Session.Set("Username", Encoding.ASCII.GetBytes("Pesho"));
+
+            this.HttpContext.Session.Set("Product1Name", Encoding.ASCII.GetBytes("Milk"));
+            this.HttpContext.Session.Set("Product2Name", Encoding.ASCII.GetBytes("Cookies"));
+
+            return View();
+        }
+
+
+        public IActionResult BuyProduct()
+        {
+            var cart = new
+            {
+                CardId = Guid.NewGuid().ToString(),
+                Items = new[]
+                {
+                    new
+                    {
+                        Name= "Milk",
+                        Price = 3.90
+                    },
+                    new
+                    {
+                        Name= "Cookies",
+                        Price = 10.90
+                    },
+                }
+            };
+
+            this.HttpContext.Session.SetString("cart", JsonConvert.ToString(cart));
+
+            return View();
+        }
+
+  
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
