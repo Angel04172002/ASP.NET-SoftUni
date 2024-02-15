6cc22524fc1dd0ce72856c6d0e223052da3b143d
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-15 21:22:34 +0200

    Add files via upload

---
+++ ASP.NET Fundamentals/4. Workshop: Forum App/ForumApp/Controllers/PostController.cs
@@ -0,0 +1,86 @@
+﻿using ForumApp.Core.Contracts;
+using ForumApp.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumApp.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService postService;
+
+        public PostController(IPostService postService)
+        {
+            this.postService = postService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = await postService.GetAllPostsAsync();
+
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var model = new PostModel();
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Add(PostModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await postService.AddPostAsync(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            PostModel? model = await postService.GetPostByIdAsync(id);
+
+            if (model == null)
+            {
+                ModelState.AddModelError("All", "Invalid Post");
+            }
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(PostModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await postService.EditPostAsync(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await postService.DeletePostAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
