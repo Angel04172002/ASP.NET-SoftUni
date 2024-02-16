5c5cf4024c3ce8a3df94b6a58082bfe32e54aa26
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-16 15:36:51 +0200

    Add files via upload

---
+++ ASP.NET Fundamentals/5. ASP.NET Identity/MVC Identity Demo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Asp.Net_Identity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Asp.Net_Identity.Controllers
+{
+    public class HomeController : BaseController
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
