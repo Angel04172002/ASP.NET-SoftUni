0499444d705fcea293ec7ae7aa8065171da6b163
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-02-26 21:48:43 +0200

    Add files via upload

---
+++ ASP.NET Advanced/1. Binding, Views and DI/AspNetCoreAdvancedDemo/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using AspNetCoreAdvancedDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System.Diagnostics;
+
+namespace AspNetCoreAdvancedDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult UploadFiles() => View();
+
+    
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(IEnumerable<IFormFile> files)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "Files");
+
+
+            foreach (var file in files.Where(f => f.Length > 0))
+            {
+                string filename = Path.Combine(path, file.FileName);
+
+                using(var fileStream = new FileStream(filename, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+
+
+            return Ok(
+                new 
+                {
+                    savedFilesLength = files.Sum(f => f.Length) 
+                });
+
+        }
+
+
+        public IActionResult Download(string filename)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "Files");
+
+            IFileProvider fileProvider = new PhysicalFileProvider(path);
+            IFileInfo fileInfo = fileProvider.GetFileInfo(filename);
+
+            var stream = fileInfo.CreateReadStream();
+            var mimeType = "application/octet-stream";
+
+            return File(stream, mimeType, filename);
+        }
+
+
+        public IActionResult GetPrice(decimal price)
+        {
+            return Ok(new
+            {
+                price
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET Advanced/1. Binding, Views and DI/AspNetCoreAdvancedDemo/Models/HomeViewModel.cs
@@ -0,0 +1,34 @@
+﻿using AspNetCoreAdvancedDemo.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreAdvancedDemo.Models
+{
+    public class HomeViewModel : IValidatableObject
+    {
+        [IsAdult(18, ErrorMessage = "Must be at least 18 years old")]
+        //[IsBefore("21/02/2024", ErrorMessage = "Date must be before 21/02/2024", DatePurpose = "Pesho")]
+        public DateTime BirthDate { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Country { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(string.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult("Name is required!");
+            }
+
+            if (string.IsNullOrEmpty(Country))
+            {
+                yield return new ValidationResult("Country is required!");
+            }
+
+            if(Name == "Pesho" && Country != "BG")
+            {
+                yield return new ValidationResult("If name is Pesho, country must be BG!");
+            }
+        }
+    }
+}
