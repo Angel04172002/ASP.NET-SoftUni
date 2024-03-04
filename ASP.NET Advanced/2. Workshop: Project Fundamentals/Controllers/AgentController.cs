9cf72eb5dc5cc5cb84127a809232b8f5a7d81c0a
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-03-04 15:39:11 +0200

    Add files via upload

---
+++ ASP.NET Advanced/2. Workshop: Project Fundamentals/Controllers/AgentController.cs
@@ -0,0 +1,27 @@
+﻿using HouseRentingSystem.Core.Models.Agent;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+    [Authorize]
+    public class AgentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Become()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Become(BecomeAgentFormModel model)
+        {
+            return RedirectToAction(nameof(HouseController.All), "Houses");
+        }
+
+    }
+}
